97e56e97c3449fa927463efed59c0d99503fd25f
Frejus <kravelife@hotmail.fr>
2014-11-17 08:34:20 +0100

    Beans Ok

--- src/main/java/org/ifi/com/muzikKloud/controller/FormSongController.java
+++ src/main/java/org/ifi/com/muzikKloud/controller/FormSongController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.ifi.com.muzikKloud.entity.Artist;
 import org.ifi.com.muzikKloud.service.ArtistService;
-import org.ifi.com.muzikKloud.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 
@@ -14,9 +13,6 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping("/addsongs")
 public class FormSongController {
-	@Autowired
-	private SongService songService;
-	
 	@Autowired
 	private ArtistService artistService;
 	
